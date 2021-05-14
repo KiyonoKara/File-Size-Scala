231c011eb38ebfa2c92d0736b65fa522aaddc161
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-05-13 18:40:02 -0700

    Feature: Made formatting for the file sizes and calculations, there are also full unit names for the file units

--- src/main/scala/FileSize.scala
+++ src/main/scala/FileSize.scala
@@ -3,21 +3,36 @@
  * File FileSize.scala
  */
 
+// I/O & New I/O
 import java.nio.file.{Files, Path, Paths}
 import java.io.IOException
 
+// Text
+import java.text.DecimalFormat
+
 object FileSize {
-  def getFileSize(file: String): String = {
+  sealed trait groups
+
+  def getFileSize(file: String, shortened: Boolean = true): String = {
     val path: Path = Paths.get(file)
 
     try {
       val bytes: Long = Files.size(path)
-      bytes.toString
+      this.readableFileSize(bytes)
     } catch {
       case io: IOException =>
         io.printStackTrace()
         io.getMessage
     }
+  }
+
+  def readableFileSize(size: Long, shortened: Boolean = true): String = {
+    if (size <= 0) return 0.toString
+
+    val units: Array[String] = if (shortened) Array("B", "KB", "MB", "GB", "TB") else Array("Bytes", "Kilobytes", "Megabytes", "Gigabytes", "Terabytes")
+    val groups: Int = (Math.log10(size) / Math.log10(1024)).asInstanceOf[Int]
+    val decimalFormat: String = s"""${new DecimalFormat("#,##0.#").format(size / Math.pow(1024, groups))} ${units(groups)}"""
+    decimalFormat
   }
 
   def main(args: Array[String]): Unit = {
