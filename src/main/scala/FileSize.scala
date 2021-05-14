01b3c47a1357e374864564c41d121376948edc4f
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-05-14 14:59:31 -0700

    Fix: Ensure the bytes of the file are Long type

--- src/main/scala/FileSize.scala
+++ src/main/scala/FileSize.scala
@@ -18,7 +18,7 @@ object FileSize {
 
     try {
       val bytes: Long = Files.size(path)
-      this.readableFileSize(bytes)
+      this.readableFileSize(bytes.asInstanceOf[Long])
     } catch {
       case io: IOException =>
         io.printStackTrace()
