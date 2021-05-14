bb582f2187a2fb2458c8555884a473148886b1a9
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-05-13 18:21:11 -0700

    Feature: Only the number of bytes is fetched

--- src/main/scala/FileSize.scala
+++ src/main/scala/FileSize.scala
@@ -3,6 +3,24 @@
  * File FileSize.scala
  */
 
-object FileSize {
+import java.nio.file.{Files, Path, Paths}
+import java.io.IOException
 
+object FileSize {
+  def getFileSize(file: String): String = {
+    val path: Path = Paths.get(file)
+
+    try {
+      val bytes: Long = Files.size(path)
+      bytes.toString
+    } catch {
+      case io: IOException =>
+        io.printStackTrace()
+        io.getMessage
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(getFileSize("src/main/scala/TestFile.scala"))
+  }
 }
