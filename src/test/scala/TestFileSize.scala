1267a680b0f68d05e117214caeebe7f5f58b8687
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-05-17 15:50:37 -0700

    Tests: More tests

--- src/test/scala/TestFileSize.scala
+++ src/test/scala/TestFileSize.scala
@@ -3,5 +3,7 @@ import FileSize.getFileSize
 object TestFileSize {
   def main(args: Array[String]): Unit = {
     println(getFileSize("src/test/scala/assets/TestFile.scala"))
+    println(getFileSize("src/test/scala/assets/TestFile.scala", isSymbol = false, "IEC", integer = true))
+    println(getFileSize("src/test/scala/assets/TestFile.scala", isSymbol = true, "JEDEC", integer = true))
   }
 }
