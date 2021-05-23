56da20fc513b5322c75c96eb68c674e6763f0a40
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-05-22 20:04:04 -0700

    Refactor: Refactor testing

--- src/test/scala/TestFileSize.scala
+++ src/test/scala/TestFileSize.scala
@@ -2,10 +2,8 @@ import FileSize.getFileSize
 
 object TestFileSize {
   def main(args: Array[String]): Unit = {
-    println(getFileSize("src/test/scala/assets/TestFile.scala"))
-    println(getFileSize("src/test/scala/assets/TestFile.scala", isSymbol = false, "IEC", integer = true))
-    println(getFileSize("src/test/scala/assets/TestFile.scala", isSymbol = true, "JEDEC", integer = true))
-
-    println(getFileSize("src/test/scala/assets/TestFile.kt"))
+    println(getFileSize("assets/TestFile.sc"))
+    println(getFileSize("assets/TestFile.sc", isSymbol = false, "IEC", integer = true))
+    println(getFileSize("assets/TestFile.sc", isSymbol = true, "JEDEC", integer = true))
   }
 }
