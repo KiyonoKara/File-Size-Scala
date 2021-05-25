d5f4c99b8ec56608fc4fbfe8f6a006e0ad63b111
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2021-05-25 08:51:26 -0700

    Tests: Update tests

--- src/test/scala/TestFileSize.scala
+++ src/test/scala/TestFileSize.scala
@@ -5,5 +5,9 @@ object TestFileSize {
     println(getFileSize("assets/TestFile.sc"))
     println(getFileSize("assets/TestFile.sc", isSymbol = false, "IEC", integer = true))
     println(getFileSize("assets/TestFile.sc", isSymbol = true, "JEDEC", integer = true))
+
+    println(getFileSize("assets/LargeTestFile.sc"))
+    println(getFileSize("assets/LargeTestFile.sc", isSymbol = false, "IEC", integer = true))
+    println(getFileSize("assets/LargeTestFile.sc", isSymbol = true, "JEDEC", integer = true))
   }
 }
